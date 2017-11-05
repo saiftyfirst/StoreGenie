59eb4ab2ac21ee86c78946935f2f72312a20657f
saifty <ahmedsaif.developer@gmail.com>
2017-11-05 08:22:49 +0800

    Promotion List without photo

--- app/src/main/java/com/example/safe/storegenie/MainActivity.java
+++ app/src/main/java/com/example/safe/storegenie/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.safe.storegenie;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import android.view.View;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button squeezeBtn;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //first commit from ankith // sd
+        context = this;
+
+        //first commit from ankith // sd // df
+        squeezeBtn = (Button) findViewById(R.id.dummyBtn);
+        squeezeBtn.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(context, PromotionActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
